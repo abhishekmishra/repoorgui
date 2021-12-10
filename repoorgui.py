50326cb7db3d7ab47992f7435dd03367df89ea4c
Abhishek Mishra <abhishekmishra3@gmail.com>
2021-12-10 11:46:02 +0530

    added toolbar

--- repoorgui.py
+++ repoorgui.py
@@ -1,6 +1,7 @@
 import git
 import PySimpleGUI as sg
 import os
+from datetime import date, datetime
 
 WORKSPACE_FOLDER = os.path.abspath(os.path.join(os.getcwd(), '..'))
 WORKSPACE_REPOS = {}
@@ -25,13 +26,21 @@ def is_git_repo(path):
 
 
 # Get all the subdirectories of the repo parent path (might call this workspace folder).
+_limit = 5 # 0 for all
 _, all_subdirs, other_files = next(os.walk(WORKSPACE_FOLDER))
 
+_count = 0
 for dir in all_subdirs:
+    if _limit > 0 and _count >= _limit:
+        break
+
     dir_abspath = os.path.abspath(os.path.join(WORKSPACE_FOLDER, dir))
     flag, repo = is_git_repo(dir_abspath)
     if flag:
         WORKSPACE_REPOS[dir] = repo
+        if _limit > 0:
+            _count += 1
+
 
 # print(WORKSPACE_REPOS)
 
@@ -41,17 +50,23 @@ for dir in all_subdirs:
 
 sg.theme('DarkAmber')   # Add a touch of color
 # All the stuff inside your window.
-layout = [[sg.Text('folder'), sg.Text('path')],
-          ]
+
+tbBtnFont = 'Helvetica 14'
+
+toolbar = [
+    sg.Button('📂', font=tbBtnFont), sg.Button('🛈', font=tbBtnFont)
+]
+
+layout = [toolbar]
 
 table_rows = []
 for repo_dir, repo in WORKSPACE_REPOS.items():
     table_rows.append([
-        str(repo_dir), str(repo.working_dir), str(repo.head.commit)
+        str(repo_dir), str(repo.working_dir), str(repo.head.commit.committed_datetime)
     ])
 
 print(table_rows)
-layout.append([sg.Table(headings=['name', 'folder', 'last_commit'], values=table_rows)])
+layout.append([sg.Table(headings=['Name', 'Dir', 'Last Commit'], values=table_rows)])
 
 # Create the Window
 window = sg.Window('Repo Org UI', layout)
@@ -60,6 +75,6 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
         break
-    print('You entered ', values[0])
+    print('You entered ', values)
 
 window.close()
