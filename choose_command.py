36a93146cc11ade2c6a4f12b66b3be18f7a5e43a
Abhishek Mishra <abhishekmishra3@gmail.com>
2021-12-17 12:36:25 +0530

    added command input

--- choose_command.py
+++ choose_command.py
@@ -1,15 +1,22 @@
 import PySimpleGUI as sg
 
 
-def choose_command_popup(title='Choose command'):
-    command = None
+def choose_command_popup(
+    title='Choose command',
+    commands = {
+        'cut': 'cut selection to clipboard',
+        'copy': 'copy selection to clipboard',
+        'paste': 'paste selection from clipboard'
+    }
+):
+    cmd = None
 
     layout = [
         [
             sg.Input(
                 enable_events=True,
-                focus = True,
-                pad = (0, 0),
+                focus=True,
+                pad=(0, 0),
                 k='command_query'
             )
         ]
@@ -21,14 +28,14 @@ def choose_command_popup(title='Choose command'):
         default_element_size=(120, 1),
         no_titlebar=True,
         grab_anywhere=True,
-        use_default_focus = False,
-        element_padding = (0, 0),
-        margins = (0, 0),
+        use_default_focus=False,
+        element_padding=(0, 0),
+        margins=(0, 0),
         finalize=True
     )
     window.bind('<Return>', '-SUBMIT-')
     window.bind('<Escape>', '-EXIT-')
-    window['command_query'].set_focus(force = True)
+    window['command_query'].set_focus(force=True)
 
     while True:
         event, values = window.read()
@@ -39,12 +46,12 @@ def choose_command_popup(title='Choose command'):
         if event in ('-EXIT-', sg.WIN_CLOSED):
             break
         elif event == '-SUBMIT-':
-            command = values['command_query']
+            cmd = values['command_query']
             break
 
     window.close()
     # sg.popup('You entered', text_input)
-    return command
+    return cmd
 
 
 if __name__ == "__main__":
