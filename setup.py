d98ec70ff95f81e0f099d2f2ce268cc8de63b2f3
Abhishek Mishra <abhishekmishra3@gmail.com>
2021-12-20 10:18:14 +0530

    package structure as per setuptools

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='repoorgui',
+    version='0.0.1',
+    packages=['repoorgui'],
+    install_requires=[
+        "pysimplegui",
+        "gitpython",
+        "platformdirs"
+    ],
+)
